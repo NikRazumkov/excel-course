a9e90d5fbb67d10b23704a20b32f67ed25e2abc4
Nik Razumkov <nik.razumkov@gmail.com>
2020-07-28 12:16:04 +0300

    finish project config

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,32 @@
-const path = require('path')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const HTMLWebpackPlugin = require('html-webpack-plugin')
-const CopyPlugin = require('copy-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const isProd = process.env.NODE_ENV === 'production';
+const isDev = !isProd;
+const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`;
+const jsLoaders = () => {
+  const loaders = [
+    {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env']
+      }
+    }
+  ];
+  if (isDev) {
+    loaders.push('eslint-loader');
+  }
+  return loaders;
+};
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   mode: 'development',
-  entry: './index.js',
+  entry: ['@babel/polyfill', './index.js'],
   output: {
-    filename: 'bundle.[hash].js',
+    filename: filename('js'),
     path: path.resolve(__dirname, 'dist')
   },
   resolve: {
@@ -17,12 +34,21 @@ module.exports = {
     alias: {
       '@': path.resolve(__dirname, 'src'),
       '@core': path.resolve(__dirname, 'src/core')
-    }
+    },
+  },
+  devtool: isDev ? 'source-map' : false,
+  devServer: {
+    port: 3000,
+    hot: isDev
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HTMLWebpackPlugin({
-      template: 'index.html'
+      template: 'index.html',
+      minify: {
+        removeComments: isProd,
+        collapseWhitespace: isProd,
+      }
     }),
     new CopyPlugin([
       {
@@ -31,29 +57,30 @@ module.exports = {
       }
     ]),
     new MiniCssExtractPlugin({
-      filename: 'bundle.[hash].css'
-    })
+      filename: filename('css'),
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.s[ac]ss$/i,
         use: [
-          MiniCssExtractPlugin.loader,
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: isDev,
+              reloadAll: true,
+            }
+          },
           'css-loader',
-          'sass-loader'
-        ],
+          'sass-loader',
+        ]
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
+        use: jsLoaders(),
       }
     ]
   }
-}
+};
