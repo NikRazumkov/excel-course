58f50790870515dd288ac258d318c91623492870
Nik Razumkov <nik.razumkov@gmail.com>
2020-10-14 07:30:54 +0300

    end lesson 37

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,33 +1,9 @@
 import {ExcelComponent} from "@core/ExelComponent";
+import {createTable} from "@/components/table/table.template";
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
   toHTML() {
-    return `
-      <div class="row">
-        <div class="row-info"></div>
-        <div class="row-data">
-          <div class="column">A</div>
-          <div class="column">B</div>
-          <div class="column">C</div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="row-info">1</div>
-        <div class="row-data">
-          <div class="cell selected" contenteditable="true" spellcheck="false">A1</div>
-          <div class="cell" contenteditable="true" spellcheck="false">B1</div>
-          <div class="cell" contenteditable="true" spellcheck="false">C1</div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="row-info">2</div>
-        <div class="row-data">
-          <div class="cell" contenteditable="true" spellcheck="false">A2</div>
-          <div class="cell" contenteditable="true" spellcheck="false">B2</div>
-          <div class="cell" contenteditable="true" spellcheck="false">C2</div>
-        </div>
-      </div>
-    `;
+    return createTable();
   }
 }
