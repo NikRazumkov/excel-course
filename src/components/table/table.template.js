556cf0b15d311a73a2574562a7cbbdc6cc24ef78
Nik Razumkov <nik.razumkov@gmail.com>
2020-10-22 07:04:10 +0300

    end lesson 45

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,15 +3,15 @@ const CODES = {
   Z: 90
 };
 
-function toCell() {
+function toCell(_, col) {
   return `
-    <div class="cell" contenteditable spellcheck="false"></div>
+    <div class="cell" contenteditable spellcheck="false" data-col="${col}"></div>
   `;
 }
 
-function toColumn(col) {
+function toColumn(col, index) {
   return `
-   <div class="column" data-type="resizable">
+   <div class="column" data-type="resizable" data-col="${index}">
         ${col}
         <div class="col-resize" data-resize="col"></div>
    </div>
