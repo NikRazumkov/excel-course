393e2d85fd27f3cd4b7b7bf08ec6d691c72d23be
Nik Razumkov <nik.razumkov@gmail.com>
2020-10-26 10:38:40 +0300

    end lesson 47

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -20,15 +20,21 @@ export class Table extends ExcelComponent {
       const $resizer = $(event.target);
       const $parent = $resizer.closest('[data-type="resizable"]');
       const coords = $parent.getCoords();
-      console.log($parent.data.col);
-
+      const type = $resizer.data.resize;
+      console.log(type);
       const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`);
 
       document.onmousemove = e => {
-        const delta = e.pageX - coords.right;
-        const value = coords.width + delta;
-        $parent.$el.style.width = value + 'px';
-        cells.forEach(el => el.style.width = value + 'px');
+        if (type === 'col') {
+          const delta = e.pageX - coords.right;
+          const value = coords.width + delta;
+          $parent.$el.style.width = value + 'px';
+          cells.forEach(el => el.style.width = value + 'px');
+        } else if (type === 'row') {
+          const delta = e.pageY - coords.bottom;
+          const value = coords.height + delta;
+          $parent.$el.style.height = value + 'px';
+        }
       };
 
       document.onmouseup = () => {
